bb6c7843b757f3b9781bf4786a374a239d8cc6ff
zzk <1019851976@qq.com>
2024-10-28 15:19:34 +0800

    zzk

--- mycpu.c
+++ mycpu.c
@@ -191,3 +191,293 @@ static void leon3_irq_ack(CPUSPARCState *env, int intno)//向 GRLIB 中断控制
     CPUState *cpu = CPU(env_cpu(env));
     grlib_irqmp_ack(env->irq_manager, cpu->cpu_index, intno);
 }
+
+static void leon3_set_pil_in(void *opaque, int n, int level){
+     /*
+     * void *opaque: 通常是设备状态 DeviceState 的指针，这里表示 CPU 设备。
+     * int level: 输入的中断优先级，用于设置中断请求。
+     *
+     */
+    DeviceState *cpu = opaque;
+    CPUState *cs = CPU(cpu);
+    CPUSPARCState *env =cpu_env(cs);
+    uint32_t pil_in = level;
+
+    assert(env != NULL);
+
+    env->pil_in = pil_in;
+    // env->pil_in == level;
+
+    if(env->pil_in && (env->interrupt_index == 0 || (env->interrupt_index & ~15) == TT_EXTINT)){
+        unsigned int i;
+
+        for(i = 15; i > 0; i --){
+            if(env->pil_in & (1 << i)){
+                int old_interrupt = env->interrupt_index;
+
+                env->interrupt_index = TT_EXTINT | i;
+                //如果找到有效位，就将 env->interrupt_index 设置为新的中断值（TT_EXTINT | i）
+                //这里的意思是高位为TT_EXTINT，低位为i
+                //例如：TT_EXTINT | i = 0x80 | 0x05 = 0x85
+                if(old_interrupt != env->interrupt_index){ //检查中断值是否发生变化
+                    trace_leon3_set_irq(i);
+                    cpu_interrupt(cs, CPU_INTERRUPT_HARD);//中断处理
+                }
+                break;
+            }
+        }
+    }else if(!env->pil_in && (env->interrupt_index & ~15) == TT_EXTINT){
+        trace_leon3_reset_irq(env->interrupt_index & 15);
+        env->interrupt_index = 0;
+        cpu_reset_interrupt(cs,CPU_INTERRUPT_HARD);
+    }
+}
+/*
+ *功能：该函数用于在异步上下文中启动 CPU。
+ *参数: CPUState *cpu: 指向 CPU 状态结构体的指针。
+ *      run_on_cpu_data data: 这是用于异步操作的数据，当前未使用。
+ * 
+ * 将 CPU 的 halted 状态设置为 0，表示 CPU 可以继续执行。
+ * 通常在 CPU 被暂停或停止后，用于重新启动 CPU 的操作。
+ */
+static void leon3_start_cpu_async_work(CPUState *cpu, run_on_cpu_data data){
+    cpu->halted = 0;
+}
+
+/*
+ *功能：启动 LEON3 CPU，通常在需要激活或恢复 CPU 时调用。
+ *参数: void *opaque: 指向设备状态的指针，通常用于传递 CPU 设备。
+ *      int n: 可能是 CPU 的编号，但在该函数中未使用。
+ *      int level: 用于检查启动条件，这里要求必须为 1。
+ * 
+ * 使用 assert 确保 level 等于 1，否则会引发错误。
+ * 调用 async_run_on_cpu，在 CPU 上异步执行 leon3_start_cpu_async_work 函数，
+ * 以重新启动 CPU。
+ */
+
+static void leon3_start_cpu(void *opaque, int n, int level){
+    DeviceState *cpu = opaque;
+    CPUState *cs = CPU(cpu);
+
+    assert(level == 1);
+    async_run_on_cpu(cs, leon3_start_cpu_async_work, RUN_ON_CPU_NULL);
+}
+
+/*
+ *功能：管理 LEON3 CPU 的中断请求。
+ *参数: CPUSPARCState *env: 指向 CPU 状态的指针，包含与 CPU 相关的状态信息。
+ *      int intno: 中断编号，表示要处理的具体中断。
+ * 
+ * 调用 leon3_irq_ack(env, intno) 函数，确认中断（acknowledge），
+ * 通常是表示已接收到该中断并准备处理。
+ * 调用 leon3_cache_control_int(env)，这可能用于处理与中断相关的缓存控制逻辑，
+ * 确保在中断处理时缓存的正确性。
+ */
+
+static void leon3_irq_manager(CPUSPARCState *env, int intno){
+    leon3_irq_ack(env, intno);
+    leon3_cache_control_int(env);
+}
+
+static void leon3_generic_hw_init(MachineState *machine)
+{
+    ram_addr_t ram_size = machine->ram_size;
+    const char *bios_name = machine->firmware ?: LEON3_PROM_FILENAME;
+    const char *kernel_filename = machine->kernel_filename;
+    SPARCCPU *cpu;
+    CPUSPARCState   *env;
+    MemoryRegion *address_space_mem = get_system_memory();
+    MemoryRegion *prom = g_new(MemoryRegion, 1);
+    int         ret;
+    char       *filename;
+    int         bios_size;
+    int         prom_size;
+    ResetData  *reset_info;
+    DeviceState *dev, *irqmpdev;
+    int i;
+    AHBPnp *ahb_pnp;
+    APBPnp *apb_pnp;
+
+    reset_info = g_malloc0(sizeof(ResetData));
+
+    for (i = 0; i < machine->smp.cpus; i++) {
+        /* Init CPU */
+        cpu = SPARC_CPU(object_new(machine->cpu_type));
+        qdev_init_gpio_in_named(DEVICE(cpu), leon3_start_cpu, "start_cpu", 1);
+        qdev_init_gpio_in_named(DEVICE(cpu), leon3_set_pil_in, "pil", 1);
+        qdev_realize(DEVICE(cpu), NULL, &error_fatal);
+        env = &cpu->env;
+
+        cpu_sparc_set_id(env, i);
+
+        /* Reset data */
+        reset_info->info[i].id = i;
+        reset_info->info[i].cpu = cpu;
+        qemu_register_reset(leon3_cpu_reset, &reset_info->info[i]);
+    }
+
+    ahb_pnp = GRLIB_AHB_PNP(qdev_new(TYPE_GRLIB_AHB_PNP));
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(ahb_pnp), &error_fatal);
+    sysbus_mmio_map(SYS_BUS_DEVICE(ahb_pnp), 0, LEON3_AHB_PNP_OFFSET);
+    grlib_ahb_pnp_add_entry(ahb_pnp, 0, 0, GRLIB_VENDOR_GAISLER,
+                            GRLIB_LEON3_DEV, GRLIB_AHB_MASTER,
+                            GRLIB_CPU_AREA);
+
+    apb_pnp = GRLIB_APB_PNP(qdev_new(TYPE_GRLIB_APB_PNP));
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(apb_pnp), &error_fatal);
+    sysbus_mmio_map(SYS_BUS_DEVICE(apb_pnp), 0, LEON3_APB_PNP_OFFSET);
+    grlib_ahb_pnp_add_entry(ahb_pnp, LEON3_APB_PNP_OFFSET, 0xFFF,
+                            GRLIB_VENDOR_GAISLER, GRLIB_APBMST_DEV,
+                            GRLIB_AHB_SLAVE, GRLIB_AHBMEM_AREA);
+
+    /* Allocate IRQ manager */
+    irqmpdev = qdev_new(TYPE_GRLIB_IRQMP);
+    object_property_set_int(OBJECT(irqmpdev), "ncpus", machine->smp.cpus,
+                            &error_fatal);
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(irqmpdev), &error_fatal);
+
+    for (i = 0; i < machine->smp.cpus; i++) {
+        cpu = reset_info->info[i].cpu;
+        env = &cpu->env;
+        qdev_connect_gpio_out_named(irqmpdev, "grlib-start-cpu", i,
+                                    qdev_get_gpio_in_named(DEVICE(cpu),
+                                                           "start_cpu", 0));
+        qdev_connect_gpio_out_named(irqmpdev, "grlib-irq", i,
+                                    qdev_get_gpio_in_named(DEVICE(cpu),
+                                                           "pil", 0));
+        env->irq_manager = irqmpdev;
+        env->qemu_irq_ack = leon3_irq_manager;
+    }
+
+    sysbus_mmio_map(SYS_BUS_DEVICE(irqmpdev), 0, LEON3_IRQMP_OFFSET);
+    grlib_apb_pnp_add_entry(apb_pnp, LEON3_IRQMP_OFFSET, 0xFFF,
+                            GRLIB_VENDOR_GAISLER, GRLIB_IRQMP_DEV,
+                            2, 0, GRLIB_APBIO_AREA);
+
+    /* Allocate RAM */
+    if (ram_size > 1 * GiB) {
+        error_report("Too much memory for this machine: %" PRId64 "MB,"
+                     " maximum 1G",
+                     ram_size / MiB);
+        exit(1);
+    }
+
+    memory_region_add_subregion(address_space_mem, LEON3_RAM_OFFSET,
+                                machine->ram);
+
+    /* Allocate BIOS */
+    prom_size = 8 * MiB;
+    memory_region_init_rom(prom, NULL, "Leon3.bios", prom_size, &error_fatal);
+    memory_region_add_subregion(address_space_mem, LEON3_PROM_OFFSET, prom);
+
+    /* Load boot prom */
+    filename = qemu_find_file(QEMU_FILE_TYPE_BIOS, bios_name);
+
+    if (filename) {
+        bios_size = get_image_size(filename);
+    } else {
+        bios_size = -1;
+    }
+
+    if (bios_size > prom_size) {
+        error_report("could not load prom '%s': file too big", filename);
+        exit(1);
+    }
+
+    if (bios_size > 0) {
+        ret = load_image_targphys(filename, LEON3_PROM_OFFSET, bios_size);
+        if (ret < 0 || ret > prom_size) {
+            error_report("could not load prom '%s'", filename);
+            exit(1);
+        }
+    } else if (kernel_filename == NULL && !qtest_enabled()) {
+        error_report("Can't read bios image '%s'", filename
+                                                   ? filename
+                                                   : LEON3_PROM_FILENAME);
+        exit(1);
+    }
+    g_free(filename);
+
+    /* Can directly load an application. */
+    if (kernel_filename != NULL) {
+        long     kernel_size;
+        uint64_t entry;
+
+        kernel_size = load_elf(kernel_filename, NULL, NULL, NULL,
+                               &entry, NULL, NULL, NULL,
+                               1 /* big endian */, EM_SPARC, 0, 0);
+        if (kernel_size < 0) {
+            kernel_size = load_uimage(kernel_filename, NULL, &entry,
+                                      NULL, NULL, NULL);
+        }
+        if (kernel_size < 0) {
+            error_report("could not load kernel '%s'", kernel_filename);
+            exit(1);
+        }
+        if (bios_size <= 0) {
+            /*
+             * If there is no bios/monitor just start the application but put
+             * the machine in an initialized state through a little
+             * bootloader.
+             */
+            write_bootloader(memory_region_get_ram_ptr(prom), entry);
+            reset_info->entry = LEON3_PROM_OFFSET;
+            for (i = 0; i < machine->smp.cpus; i++) {
+                reset_info->info[i].cpu->env.pc = LEON3_PROM_OFFSET;
+                reset_info->info[i].cpu->env.npc = LEON3_PROM_OFFSET + 4;
+            }
+        }
+    }
+
+    /* Allocate timers */
+    dev = qdev_new(TYPE_GRLIB_GPTIMER);
+    qdev_prop_set_uint32(dev, "nr-timers", LEON3_TIMER_COUNT);
+    qdev_prop_set_uint32(dev, "frequency", CPU_CLK);
+    qdev_prop_set_uint32(dev, "irq-line", LEON3_TIMER_IRQ);
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(dev), &error_fatal);
+
+    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, LEON3_TIMER_OFFSET);
+    for (i = 0; i < LEON3_TIMER_COUNT; i++) {
+        sysbus_connect_irq(SYS_BUS_DEVICE(dev), i,
+                           qdev_get_gpio_in(irqmpdev, LEON3_TIMER_IRQ + i));
+    }
+
+    grlib_apb_pnp_add_entry(apb_pnp, LEON3_TIMER_OFFSET, 0xFFF,
+                            GRLIB_VENDOR_GAISLER, GRLIB_GPTIMER_DEV,
+                            0, LEON3_TIMER_IRQ, GRLIB_APBIO_AREA);
+
+    /* Allocate uart */
+    dev = qdev_new(TYPE_GRLIB_APB_UART);
+    qdev_prop_set_chr(dev, "chrdev", serial_hd(0));
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(dev), &error_fatal);
+    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, LEON3_UART_OFFSET);
+    sysbus_connect_irq(SYS_BUS_DEVICE(dev), 0,
+                       qdev_get_gpio_in(irqmpdev, LEON3_UART_IRQ));
+    grlib_apb_pnp_add_entry(apb_pnp, LEON3_UART_OFFSET, 0xFFF,
+                            GRLIB_VENDOR_GAISLER, GRLIB_APBUART_DEV, 1,
+                            LEON3_UART_IRQ, GRLIB_APBIO_AREA);
+}
+
+/*
+ *功能：设置和注册 LEON3 通用机器的基本信息
+ *描述 (mc->desc)：设置机器的描述为 "Leon-3 generic"。
+ *初始化函数 (mc->init)：指定机器的初始化函数为 leon3_generic_hw_init，
+ * 这意味着当该机器被创建时，会调用该函数来进行硬件初始化。
+ * 
+ * 默认 CPU 类型 (mc->default_cpu_type)：设置默认的 CPU 类型为 LEON3，
+ * 使用 SPARC_CPU_TYPE_NAME 宏来获取对应的类型名称。
+ * 
+ * 默认 RAM ID (mc->default_ram_id)：设置默认 RAM 的标识符为 "leon3.ram"，用于 RAM 的初始化。
+ * 
+ * 最大 CPU 数量 (mc->max_cpus)：指定该机器支持的最大 CPU 数量，取值来自 MAX_CPUS。
+ */
+static void leon3_generic_machine_init(MachineClass *mc){
+    mc->desc = "leon-3 generic";
+    mc->init = leon3_generic_hw_init;
+    mc->default_cpu_type = SPARC_CPU_TYPE_NAME("LEON3");
+    mc->default_ram_id = "leon3.ram";
+    mc->max_cpus = MAX_CPUS;
+}
+
+
+DEFINE_MACHINE("leon3_generic", leon3_generic_machine_init)
